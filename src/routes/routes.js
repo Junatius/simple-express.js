9e14f716e2e1ae1b1ccd17679a891244c4f749af
Junatius <cjuniar0@gmail.com>
2023-10-08 13:02:45 +0800

    fix: change method

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,8 +5,8 @@ const categoriesController = require('../controllers/categoriesController');
 const router = express.Router();
 
 router.get('/films', filmsController.getAllFilms);
-router.post('/film/:id', filmsController.getFilmById);
+router.get('/film/:id', filmsController.getFilmById);
 router.get('/categories', categoriesController.getAllCategories);
-router.post('/films/:category', filmsController.getFilmsByCategory);
+router.get('/films/:category', filmsController.getFilmsByCategory);
 
 module.exports = router;
