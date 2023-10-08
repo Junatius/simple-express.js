6e36aa255797725ca60878b884c83becd290abff
Junatius <cjuniar0@gmail.com>
2023-10-08 11:39:46 +0800

    fix: change response of categories

--- src/models/categoriesModel.js
+++ src/models/categoriesModel.js
@@ -3,7 +3,7 @@ const pool = require('../config/database');
 const getAllCategories = async () => {
     const client = await pool.connect();
     try {
-        const result = await client.query('SELECT * FROM category');
+        const result = await client.query('SELECT category_id, name FROM category');
         return result.rows;
     } finally {
         client.release();
